239140305bb3cc8ec756c873c61df955c0842751
Rodolfo <rodolfougaldeochoa@gmail.com>
2023-11-08 19:51:12 -0600

    :lipstick: [WIP] se agregan fuentes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
       './node_modules/flowbite/**/*.js'
   ],
   theme: {
-    extend: {},
+      extend: {
+          fontFamily: {
+              'body': ['Roboto Mono', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui'],
+              'sans': ['Roboto Mono', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui'],
+              'serif': ['Roboto Mono', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui'],
+              'mono': ['Roboto Mono', 'ui-sans-serif', 'system-ui', '-apple-system', 'system-ui']
+          },
+     },
   },
   plugins: [
       require('flowbite/plugin')
